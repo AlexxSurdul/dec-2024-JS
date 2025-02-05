50c1321d012436641abf1d38d061be439b9f0e51
AlexxSurdul <alexxsurdul@gmail.com>
2025-02-05 15:59:38 +0200

    commit lesson1, hw1

---
+++ Lesson1/2.variables types/scripts/main.js
@@ -0,0 +1,30 @@
+console.log('any text from main.js');
+
+//типи змінних:
+// number
+// string
+// boolean
+
+//декларація змінної, але зараз var ніхто не використовує - використовують let
+// var a;
+//ініціалізація змінної
+// a = 'hello okten';
+
+//блок ініціалізації створюється фігурними дужками
+//var - можна звертатись поза межами блоку ініціалізації (знижує безпеку)
+//let - можна звертатись лише в межах блоку ініціалізації (відповідно краща безпека)
+// {
+//     let a = 'hello okten';
+//     var b = 'hello world';
+// }
+// console.log(b);
+// console.log(a);
+
+const PI = 3.14;
+console.log(PI);
+// PI = 123;
+
+let a = 'ufhgasfdh';
+console.log(a);
+a = 'okten';
+console.log(a);
