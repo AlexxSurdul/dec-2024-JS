1ceda514a45bea56c807ca6c659b5a2bd30a5806
AlexxSurdul <alexxsurdul@gmail.com>
2025-02-21 20:16:11 +0200

    changed HW8 main.js

--- HW8/main.js
+++ HW8/main.js
@@ -3,6 +3,77 @@
 // Додати перевірки на undefined, null, NaN.
 //     Подумати і реалізувати логіку, в якій кінцевий об'єкт буде мати функції, які в нього були до цього моменту.
 
+//--------об'єкт з функцією всередині------------
+const users = [
+    {
+        name: 'John',
+        email: 'john@example.com',
+        password: 'password123',
+        age: 21,
+        nameUpperCase() {
+            return this.name.toUpperCase()
+        },
+    },
+    {
+        name: 'Anna',
+        email: 'anna@example.com',
+        password: 'password000',
+        age: 25,
+        nameUpperCase() {
+            return this.name.toUpperCase()
+        },
+    }
+];
+
+// console.log(users.nameUpperCase()); //перевірка функції
+console.log(users);
+
+console.log('-----');
+// //---------функція копіювання, не працює для масиву об'єктів з функціями-------------------
+//
+// function objFnCopier (objToCopying) {
+//     let objTemp = JSON.parse(JSON.stringify(objToCopying));
+//     Object.entries(objToCopying).forEach(([key, value]) => {
+//        if (typeof value === 'function') {
+//            objTemp[key] = value;
+//        }
+//     });
+//     return objTemp;
+// }
+//
+// let usersCopy = objFnCopier (users);
+// console.log(usersCopy);
+// console.log(users.nameUpperCase === usersCopy.nameUpperCase);
+
+//---------спроба скопіювати масив об'єктів---------------
+
+function objFnArrCopier(objToCopying) {
+
+    if (Array.isArray(objToCopying)) {
+        return objToCopying.map(item => objFnArrCopier(item));
+    }
+
+    if (typeof objToCopying !== 'object' || objToCopying === null) {
+        return objToCopying;
+    }
+
+    let objTemp = JSON.parse(JSON.stringify(objToCopying));
+
+
+    Object.entries(objToCopying).forEach(([key, value]) => {
+        if (typeof value === 'function') {
+            objTemp[key] = value;
+        }
+    });
+
+    return objTemp;
+}
+
+
+let usersArrCopy = objFnArrCopier(users);
+console.log(usersArrCopy);
+
+console.log('-----');
 
 // #iz6emEsP2BA
 // - є масив
@@ -14,5 +85,11 @@ let coursesAndDurationArray = [
     {title: 'FullStack', monthDuration: 7},
     {title: 'Frontend', monthDuration: 4}
 ];
+
 // за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration
 //     Зробити все ВИКЛЮЧНО за допомоги інлайн конструкції
+
+let newArray = coursesAndDurationArray.map((item, index) => ({...item, id: index + 1}));
+
+
+console.log(newArray);
